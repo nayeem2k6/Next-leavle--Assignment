745ced211f6e9a889c7b022a6ca8e7be9e4c61ce
Nayeem Babu <nayeembabu2k6@gmail.com>
2026-09-02 22:12:00 -0700

    Question -2 answer

--- answer.js
+++ answer.js
@@ -3,3 +3,31 @@ function describeValue(value){
 }
 console.log (describeValue("Hello"));
 console.log (describeValue(""));
+
+
+
+
+
+// QUESTION 2 
+
+function getDayType(day){
+    switch(day){
+        case "friday":
+        case"saturday":
+         return "weekend"; 
+
+
+         case "sunday":
+            case 'monday':
+                return "weekday";
+                case "tuesday":
+                    case "wendesday":
+                        case "thursday":
+                            return "working Day";
+                            default : 
+                            return"invalid day";
+
+    }
+}
+
+console.log(getDayType("friday"));
